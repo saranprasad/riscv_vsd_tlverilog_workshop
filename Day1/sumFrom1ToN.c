99f567751aa28e7423ef65f3f9dcba23aa53d73c
saranprasad <saranprasadkj@gmail.com>
2025-08-13 02:06:37 +0530

    Create sumFrom1ToN.c

---
+++ Day1/sumFrom1ToN.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+        int i, sum = 0, n = 100;
+        for (i=1; i <= n; ++i)
+        {
+                sum += i;
+        }
+
+        printf("The sum of the numbers from 1 to %d is %d\n", n, sum);
+        return 0;
+}
